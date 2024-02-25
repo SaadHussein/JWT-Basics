b4c8308c82183214c20dd79b6b9bb0b97b45ef92
SaadHussein <sa3dhusseein@gmail.com>
2024-02-26 00:06:17 +0200

    Fix bug and finish project

--- middleware/auth.js
+++ middleware/auth.js
@@ -13,7 +13,7 @@ const authenticationMiddleware = async (req, res, next) => {
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
         const { id, username } = decoded;
-        req.user = { id, user };
+        req.user = { id, username };
         next();
     } catch (err) {
         throw new UnauthenticatedError('Not Authorized To Access This Route.');
--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const LoginRouter = express.Router();
 const { login, dashboard } = require('../controllers/main');
+const authenticationMiddleware = require('../middleware/auth');
 
 LoginRouter.route('/login').post(login);
-LoginRouter.route('/dashboard').get(dashboard);
+LoginRouter.route('/dashboard').get(authenticationMiddleware, dashboard);
 
 module.exports = LoginRouter;
